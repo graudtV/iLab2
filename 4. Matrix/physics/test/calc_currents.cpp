e6037eda9f923d9f92867031b283a7fc987149c8
graudtV <55806873+graudtV@users.noreply.github.com>
2021-01-06 05:25:30 +0300

    Physics initial commit

---
+++ 4. Matrix/physics/test/calc_currents.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include "electric_circuit.h"
+#include "scan.h"
+
+void usage_error(const char *errmsg)
+{
+	std::cerr << errmsg << std::endl;
+	exit(EXIT_FAILURE);
+}
+
+int main()
+try {
+	Physics::ElectronicCircuit circuit;
+
+	while (std::cin) {
+		std::string line;
+		int node1, node2;
+		double R;
+		double E = 0;
+
+		std::getline(std::cin, line);
+		scn::scanner scan(line);
+
+		if (line.size() == 0)
+			break;
+		scan >> node1 >> scn::skip_spaces >> "--" >> scn::skip_spaces >> node2
+			>> scn::skip_spaces >> "," >> scn::skip_spaces >> R;
+		try {
+			scan >> scn::skip_spaces >> scn::end_of_text;
+			printf("resistor\n");
+			circuit.add_component(node1, node2, Physics::make_resistor(R));
+			continue;
+		} catch (scn::scan_error& e) {}
+		printf("voltage\n");
+		scan >> ";" >> scn::skip_spaces >> E >> scn::skip_spaces
+			>> "V" >> scn::skip_spaces >> scn::end_of_text;
+		circuit.add_component(node1, node2, Physics::make_voltage_src(R, E)); // resistor is voltage source with E = 0
+	}
+
+	for (int comp_idx = 0; comp_idx != circuit.ncomponents(); ++comp_idx) {
+		auto comp_data = circuit.get_component_data(comp_idx);
+		std::cout << comp_data.node1 << " -- " <<comp_data.node2 << ": "
+			<< circuit.get_current(comp_idx) << " A\n";
+	}
+	return 0;
+} catch (scn::scan_error& e) {
+	std::cerr << "error while parsing input data: " << e.what() << std::endl;
+}
